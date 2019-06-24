b814c4112b64fbeaaeb3a3c0bfd4d34295448cae
Administrator <JYT>
2019-06-24 21:41:29 +0800

    第二次提交，修改自己的项目

--- src/com/jyt/testGit/HelloWorld.java
+++ src/com/jyt/testGit/HelloWorld.java
@@ -3,7 +3,7 @@ package com.jyt.testGit;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World，修改自己的项目");
 
 	}
 
