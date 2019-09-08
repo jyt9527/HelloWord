3608b3053d96a8406dd5886872057512f6ea51d0
jyt9527 <jingyuntao9527@163.com>
2019-09-08 16:11:16 +0800

    tijiao

--- src/com/jyt/testGit/HelloWorld.java
+++ src/com/jyt/testGit/HelloWorld.java
@@ -3,7 +3,7 @@ package com.jyt.testGit;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World，2019.9.8");
+		System.out.println("Hello World，2019.9.8，用户一修改");
 
 	}
 
