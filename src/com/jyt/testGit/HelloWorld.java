410537e9a100f2cb4e9c48c9919120a19bb6cbf4
jyt9527 <jingyuntao9527@163.com>
2019-09-08 15:31:11 +0800

    2019.9.8的第一次提交

--- src/com/jyt/testGit/HelloWorld.java
+++ src/com/jyt/testGit/HelloWorld.java
@@ -3,7 +3,7 @@ package com.jyt.testGit;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World，修改自己的项目");
+		System.out.println("Hello World，修改自己的项目，2019.9.8");
 
 	}
 
